87ccf8ff1703f8dd927a8f01fd2cf81b673f9707
abouzaid <mohmmdabouzaid@gmail.com>
2022-02-17 17:27:22 +0200

    add sign up

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+  register(user: any) {
+
+    return this.http.post('http://localhost:3000/doctors/', user);
+}
+}
